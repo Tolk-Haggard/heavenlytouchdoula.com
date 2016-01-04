27d8ac77520d8638ec336a3dfe1481f66c0e4a37
Brian Haggard <brihagg@gmail.com>
2016-01-03 18:06:16 -0600

    Retry adding images to modal window if initial load does not work

--- assests/js/portfolio.js
+++ assests/js/portfolio.js
@@ -3,19 +3,25 @@ $(document).ready(function(){/* copy loaded thumbnails into carousel */
 $('.row .thumbnail').on('load', function() {
 
 }).each(function(i) {
-  if(this.complete) {
+  attachModal(this, i);
+});
+
+function attachModal(thumb, counter) {
+  if(thumb.complete && $('.carousel-inner').children().length >= counter) {
     var item = $('<div class="item"></div>');
-    var itemDiv = $(this).parents('div');
-    var title = $(this).parent('a').attr("title");
+    var itemDiv = $(thumb).parents('div');
+    var title = $(thumb).parent('a').attr("title");
 
     item.attr("title",title);
     $(itemDiv.html()).appendTo(item);
     item.appendTo('.carousel-inner');
-    if (i==0){ // set first item active
+    if (counter==0){ // set first item active
      item.addClass('active');
     }
+  } else {
+      setTimeout(function(){attachModal(thumb, counter);}, 1000);
   }
-});
+}
 
 /* activate the carousel */
 $('#modalCarousel').carousel({interval:false});
